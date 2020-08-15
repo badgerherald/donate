0eec63c37fda64712900f67ea0eb7abce9a88364
Will Haynes <haynes24@gmail.com>
2020-08-15 12:55:32 -0500

    adding wordpress support / php libs

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,19 +1,22 @@
 import { Config } from '@stencil/core';
 import { sass } from '@stencil/sass';
 
-export const config : Config = {
-	srcDir: 'src',
-	globalStyle: 'src/assets/sass/foundations.scss',
-
-	outputTargets: [
-		{ type: 'www', serviceWorker: null }
-	],
-	plugins: [
+export const config: Config = {
+  namespace: 'donate',
+  taskQueue: 'async',
+  outputTargets: [
+    {
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      copy: [
+        { src: '**/*.php' },
+        { src: 'assets/' },
+        ] 
+    }
+  ],
+  plugins: [
 		sass()
 	],
 };
 
-exports.devServer = {
-	root: "www",
-	watchGlob: ["./src/client/**"]
-}
+
