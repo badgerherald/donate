3a7c30dfac876a706bab985c410e8cb3efbc2278
Will Haynes <haynes24@gmail.com>
2020-09-12 20:16:15 -0500

    fixing grid

--- src/index.php
+++ src/index.php
@@ -16,23 +16,214 @@
       padding: 0;
     }
     body {
-      margin: 20px 20px;
+      margin: 20px 0px;
       background: #eff4f6;
     }
 
-    
-    @media (min-width: 600px) {
-      body {
-        margin: 40px 100px;
-      }
-    }
+.container {
+ width:calc(100% * (((((var(--columnWidth) * 3) + (var(--gutterWidth) * (3 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+ position:relative
+}
+.container,
+.container * {
+ --breakpointColumns:3;
+ --additionalGutter:0;
+ --gutterWidth:20;
+ --columnWidth:80
+}
+.container *,
+.container:after,
+.container:before {
+ --breakpointColumns:3;
+ --additionalGutter:0
+}
+.container .wrapper {
+ min-width:280px;
+ max-width:92%;
+ margin:0 auto;
+ position:relative
+}
+@media (min-width: 620px) {
+ .container {
+  width:calc(100% * (((((var(--columnWidth) * 6) + (var(--gutterWidth) * (6 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+  position:relative
+ }
+ .container .wrapper {
+  min-width:580px;
+  max-width:95%
+ }
+ .container,
+ .container * {
+  --breakpointColumns:6;
+  --additionalGutter:0;
+  --gutterWidth:20;
+  --columnWidth:80
+ }
+ .container *,
+ .container:after,
+ .container:before {
+  --breakpointColumns:6;
+  --additionalGutter:0
+ }
+}
+@media (min-width: 930px) {
+ .container {
+  width:calc(100% * (((((var(--columnWidth) * 9) + (var(--gutterWidth) * (9 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+  position:relative
+ }
+ .container .wrapper {
+  min-width:880px
+ }
+ .container,
+ .container * {
+  --breakpointColumns:9;
+  --additionalGutter:0;
+  --gutterWidth:20;
+  --columnWidth:80
+ }
+ .container *,
+ .container:after,
+ .container:before {
+  --breakpointColumns:9;
+  --additionalGutter:0
+ }
+}
+@media (min-width: 1240px) {
+ .container {
+  width:calc(100% * (((((var(--columnWidth) * 12) + (var(--gutterWidth) * (12 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+  position:relative
+ }
+ .container .wrapper {
+  min-width:1180px;
+  max-width:90%
+ }
+ .container,
+ .container * {
+  --breakpointColumns:12;
+  --additionalGutter:0;
+  --gutterWidth:20;
+  --columnWidth:80
+ }
+ .container *,
+ .container:after,
+ .container:before {
+  --breakpointColumns:12;
+  --additionalGutter:0
+ }
+}
+@media (min-width: 1580px) {
+ .container {
+  width:calc(100% * (((((var(--columnWidth) * 15) + (var(--gutterWidth) * (15 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+  position:relative
+ }
+ .container .wrapper {
+  min-width:1480px;
+  max-width:80%
+ }
+ .container,
+ .container * {
+  --breakpointColumns:15;
+  --additionalGutter:0;
+  --gutterWidth:20;
+  --columnWidth:80
+ }
+ .container *,
+ .container:after,
+ .container:before {
+  --breakpointColumns:15;
+  --additionalGutter:0
+ }
+}
+.container {
+ margin:24px 0;
+ width:100%;
+ position:relative
+}
+.container.mobile,
+.container.tablet,
+.container.desktop,
+.container.xl {
+ display:none
+}
+.container.mobile {
+ display:block
+}
+.container.black {
+ background:#191919
+}
+@media (min-width: 930px) {
+ .container.mobile,
+ .container.desktop,
+ .container.xl {
+  display:none
+ }
+ .container.tablet {
+  display:block
+ }
+}
+@media (min-width: 1240px) {
+ .container.mobile,
+ .container.tablet,
+ .container.xl {
+  display:none
+ }
+ .container.desktop {
+  display:block
+ }
+}
+@media (min-width: 1580px) {
+ .container.mobile,
+ .container.tablet,
+ .container.desktop {
+  display:none
+ }
+ .container.xl {
+  display:block
+ }
+}
+
+.donate-form *,
+.donate-form:after,
+.donate-form:before {
+ --breakpointColumns:3;
+ --additionalGutter:2
+}
+@media (min-width: 620px) {
+ .donate-form {
+  width:calc(100% * (((((var(--columnWidth) * 6) + (var(--gutterWidth) * (6 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 2)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))));
+  position:relative;
+  width:calc(100% * (((((var(--columnWidth) * 6) + (var(--gutterWidth) * (6 - 1 + 0)))) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter)))))) + (((var(--gutterWidth) * 0)) / (((var(--columnWidth) * var(--breakpointColumns)) + (var(--gutterWidth) * (var(--breakpointColumns) - 1 + var(--additionalGutter))))))))
+ }
+ .donate-form *,
+ .donate-form:after,
+ .donate-form:before {
+  --breakpointColumns:6;
+  --additionalGutter:2
+ }
+}
+
+@media (min-width: 620px) {
+  .container .wrapper .donate-form {
+    position: relative;
+  }
+  .container .wrapper .donate-form *, .container .wrapper .donate-form:after, .container .wrapper .donate-form:before {
+    --breakpointColumns: 6;
+    --additionalGutter:2;
+  }
+}
+
+
   </style>
 
 </head>
 <body>
+  <div class="container">
+    <div class="wrapper">
   <?php 
   echo do_shortcode( '[badgerherald_donation_form]');
   ?>
+    </div>
+  </div>
   
 </body>
 </html>
--- stencil.config.ts
+++ stencil.config.ts
@@ -1,35 +1,29 @@
-import { Config } from '@stencil/core';
-import { sass } from '@stencil/sass';
+import { Config } from "@stencil/core"
+import { sass } from "@stencil/sass"
 
 export const config: Config = {
-  namespace: 'donate',
-  outputTargets: [
-    {
-      type: 'dist',
-      esmLoaderPath: '../loader',
-      copy: [
-        { src: 'lib/**/*' },
-        { src: 'lib/**/*.php' },
-        { src: 'functions.php' },
-        { src: 'assets/' },
-        ] 
-    },
-    {
-      type: 'www',
-      buildDir: 'app',
-      dir:'wp-content/themes/donate-test/',
-      copy: [
-        { src: 'lib/**/*' },
-        { src: 'functions.php' },
-        { src: 'index.php' },
-        { src: 'style.css' },
-        { src: 'assets/' },
-        ] 
-    }
-  ],
-  plugins: [
-		sass()
-  ]
-};
-
-
+	namespace: "donate",
+	outputTargets: [
+		{
+			type: "dist",
+			esmLoaderPath: "../loader",
+			copy: [
+				{ src: "lib/**/*" },
+				{ src: "lib/**/*.php" },
+				{ src: "functions.php" },
+			],
+		},
+		{
+			type: "www",
+			buildDir: "app",
+			dir: "wp-content/themes/donate-test/",
+			copy: [
+				{ src: "lib/**/*" },
+				{ src: "functions.php" },
+				{ src: "index.php" },
+				{ src: "style.css" },
+			],
+		},
+	],
+	plugins: [sass({ injectGlobalPaths: ["src/global/sass/foundations.scss"] })],
+}
